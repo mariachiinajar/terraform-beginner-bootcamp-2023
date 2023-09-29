f8073dd2241c206cead56ef9dc725447ad9ddcba
Mariachi in and out of a jar <mariachiinajar@gmail.com>
2023-09-29 10:47:46 +0000

    #27 Enabled static website hosting, uploaded index and error.html via terraform

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "bucket_name" {
-    description = "Bucket name for our static website hosting"
-    value = module.terrahouse_aws.bucket_name
+  description = "Bucket name for our static website hosting"
+  value       = module.terrahouse_aws.bucket_name
+}
+
+output "s3_website_endpoint" {
+  description = "website endpoint for the statically hosted website"
+  value       = module.terrahouse_aws.website_endpoint
 }
