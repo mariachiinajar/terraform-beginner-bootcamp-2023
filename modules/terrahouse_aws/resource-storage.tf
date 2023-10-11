7d64944e9497ef252411756858ee99413dc6dc2c
Mariachi in and out of a jar <mariachiinajar@gmail.com>
2023-10-11 19:06:14 +0000

    Put things back to normal

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -32,20 +32,20 @@ resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   # source = var.index_html_filepath
-  source = "${path.root}${var.index_html_filepath}"
+  source = var.index_html_filepath
   content_type = "text/html"
 
-  etag   = filemd5("${path.root}${var.index_html_filepath}")
+  etag   = filemd5(var.index_html_filepath)
 }
 
 
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = "${path.root}${var.error_html_filepath}"
+  source = var.error_html_filepath
   content_type = "text/html"
 
-  etag   = filemd5("${path.root}${var.error_html_filepath}")
+  etag   = filemd5(var.error_html_filepath)
 }
  
 resource "aws_s3_bucket_policy" "bucket_policy" {
