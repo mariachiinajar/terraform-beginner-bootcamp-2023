52ad64da50f92a95b80764bee317ca11911ec05a
Mariachi in and out of a jar <mariachiinajar@gmail.com>
2023-10-14 20:06:28 +0000

    #52 Tested our terrahome on terratowns test town Missingo

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_origin_access_control" "default" {
-  name                              = "OAC ${var.bucket_name}"
-  description                       = "Origin Access Controls for Static Website Hosting ${var.bucket_name}"
+  name                              = "OAC ${aws_s3_bucket.website_bucket.bucket}"
+  description                       = "Origin Access Controls for Static Website Hosting ${aws_s3_bucket.website_bucket.bucket}"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
@@ -12,14 +12,14 @@ locals {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_nameg
+    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Static website hosting for: ${var.bucket_name}"
+  comment             = "Static website hosting for: ${aws_s3_bucket.website_bucket.bucket}"
   default_root_object = "index.html"
 
   #   aliases = ["mysite.example.com", "yoursite.example.com"]
--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
+  # bucket = var.bucket_name
+  # We want to assign a random bucket name.
 
   tags = {
     UserUuid = var.user_uuid
