8091e6690a1cef9e69059fd7cfc5c6506a95ad5e
Mariachi in and out of a jar <mariachiinajar@gmail.com>
2023-10-14 12:12:07 +0000

    Troublehsooting - failed to create home resource, status_code: 401, status: 401 Unauthorized, body: map[err:a1000 Failed to authenicate, bearer token invalid and/or teacherseat_user_uuid invalid]

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint  = "http://localhost:4567"
+  endpoint  = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -34,3 +34,16 @@ provider "terratowns" {
 #   content_version     = var.content_version
 #   assets_path         = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name        = "Nomadiachi's nomadic life"
+  description = <<DESCRIPTION
+    Mariachi works nomadically so popping in and checking out here and there.
+    Pictures of some memorable moments, quick snapshots of days in and days out. 
+    Come join my nomadinary journey? :D 
+DESCRIPTION
+  # domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name     = "3xf332sdfs.cloudfront.net"
+  town            = "the-nomad-pad"
+  content_version = 2
+}
